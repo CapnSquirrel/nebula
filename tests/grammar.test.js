82f4f09f024c2eed5cd918668a0e365ff38af421
Brian Joerger <joergerbrian@gmail.com>
2018-03-20 18:50:16 -0700

    added a file extension check for grammar test

--- tests/grammar.test.js
+++ tests/grammar.test.js
@@ -6,11 +6,13 @@ const TEST_DIR = 'tests/data/good-programs';
 
 describe('The grammar', () => {
   fs.readdirSync(TEST_DIR).forEach((name) => {
-    test(`should parse ${name} without errors`, (done) => {
-      assert.doesNotThrow(() => {
-        parse(fs.readFileSync(`${TEST_DIR}/${name}`, 'utf-8'));
+    if (name.endsWith('.star')) {
+      test(`should parse ${name} without errors`, (done) => {
+        assert.doesNotThrow(() => {
+          parse(fs.readFileSync(`${TEST_DIR}/${name}`, 'utf-8'));
+        });
+        done();
       });
-      done();
-    });
+    }
   });
 });
