63a2f576ac0a148fc0c9edafa988c8cbb4e9fcb2
Brian Joerger <joergerbrian@gmail.com>
2018-04-19 18:13:33 -0700

    added symantic check for Number.js

--- ast/traits/Number.js
+++ ast/traits/Number.js
@@ -1,3 +1,14 @@
 const Trait = require('../bases/Trait.js');
 
-module.exports = class Number extends Trait {};
+module.exports = class Number extends Trait {
+  analyze() {
+    // need to ensure the only arg is a numlit
+    if (this.args.length !== 1) {
+      throw new Error('Number must have 1 argument');
+    } else if (typeof this.args[0].value !== 'number') {
+      throw new Error('Must provide a number');
+    } else if (this.body.length !== 0) {
+      throw new Error('Number cannot have a body');
+    }
+  }
+};
