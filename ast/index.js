fa7494595a9eb4160ccbd444f12c2285d026fe70
cazinge <eddie.azinge@gmail.com>
2018-05-09 01:25:41 -0700

    reverted module.exports change

--- ast/index.js
+++ ast/index.js
@@ -1,38 +1,15 @@
-// const requireDir = (directory) => {
-//   const normalizedPath = require('path').join(__dirname, directory);
-//   const exports = {};
-//   require('fs')
-//     .readdirSync(normalizedPath)
-//     .forEach((file) => {
-//       const item = require(`./${directory}/${file}`);
-//       exports[item.name] = item;
-//     });
-//   return exports;
-// };
-//
-// module.exports = requireDir('.');
 /* eslint-disable global-require, import/no-dynamic-require */
-module.exports = {
-  Access: require('./Access'),
-  Accessor: require('./Accessor'),
-  Argument: require('./Argument'),
-  BooleanLiteral: require('./BooleanLiteral'),
-  Condition: require('./Condition'),
-  Conditional: require('./Conditional'),
-  Control: require('./Control'),
-  Coordinate: require('./Coordinate'),
-  Evaluate: require('./Evaluate'),
-  Function: require('./Function'),
-  Initialize: require('./Initialize'),
-  Link: require('./Link'),
-  Location: require('./Location'),
-  NumericLiteral: require('./NumericLiteral'),
-  Origin: require('./Origin'),
-  Parameter: require('./Parameter'),
-  Program: require('./Program'),
-  Result: require('./Result'),
-  Return: require('./Return'),
-  SetParameter: require('./SetParameter'),
-  StringLiteral: require('./StringLiteral'),
+const requireDir = (directory) => {
+  const normalizedPath = require('path').join(__dirname, directory);
+  const exports = {};
+  require('fs')
+    .readdirSync(normalizedPath)
+    .forEach((file) => {
+      const item = require(`./${directory}/${file}`);
+      exports[item.name] = item;
+    });
+  return exports;
 };
 /* eslint-enable global-require, import/no-dynamic-require */
+
+module.exports = requireDir('.');
