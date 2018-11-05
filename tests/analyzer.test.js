1a27a4b16a19684e706080656bcbc53d38700c83
Justin Torres <jtorre39@lion.lmu.edu>
2018-11-04 23:30:47 -0800

    fix for windows incompatible filenames

--- tests/analyzer.test.js
+++ tests/analyzer.test.js
@@ -9,8 +9,8 @@ const ERROR_PROGRAMS = 'tests/data/semantic-errors';
 describe('The semantic analyzer', () => {
   fs.readdirSync(ERROR_PROGRAMS).forEach((name) => {
     if (name.endsWith('.error')) {
-      test(`detects a ${name.replace(/[^a-z]/g, ' ')}`, (done) => {
-        const errorPattern = RegExp(name.replace('.error', '').replace(/-/g, ' '), 'i');
+      test(`detects a ${name.replace(/[^a-z_]/gi, ' ').replace(/_/g, ': ')}`, (done) => {
+        const errorPattern = RegExp(name.replace('.error', '').replace(/-/g, ' ').replace(/_/g, ': '), 'i');
         const text = fs.readFileSync(`${ERROR_PROGRAMS}/${name}`, 'utf-8');
         chai.assert.throws(() => analyzeProgram(text), errorPattern);
         done();
